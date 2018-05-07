a266efd6ff6a2f7196ee2d80464aecc2274c9b95
Muhammad-Salman-Sheikh <37584841+Muhammad-Salman-Sheikh@users.noreply.github.com>
2018-05-07 16:27:53 +0300

    Create ASD.py

---
+++ ASD.py
@@ -0,0 +1,23 @@
+import sys
+
+def Interpreter(program , stream):
+	'''
+	Initial values for both integral and string results
+	'''
+	intValue = 0
+	strValue = ''
+	for i in  program:
+		if prgram == '':
+			a = ''
+			for i in range(1,100):
+				if i % 3 == 0 and i % 5 == 0:
+					a += 'FizzBuzz '
+				elif i % 3 == 0:
+					a += 'Fizz '
+				elif i % 5 == 0:
+					a += 'Buzz '
+				else :
+					a += '' + str(i) + ' '
+			return '\n'.join(a.split(" "))
+		elif program = ' ':
+			pass
